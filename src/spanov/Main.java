7a002ef2d6088f0343f5efb1bf370f9fa85f11aa
Panov Stanislav <ju5tas@ya.ru>
2013-12-12 22:13:22 +0700

    Reformat

--- src/spanov/Main.java
+++ src/spanov/Main.java
@@ -4,11 +4,11 @@ import static spanov.ArrayGroupSort.Order;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = {1, 0, 3, 2, 6, 5,
-                1, 8, 4, 8, 5, 2,
-                0, 5, 3, 2, 3, 5,
-                6, 5, 1, 8, 4, 1,
-                7, 2, 5, 2, 1, 9,
+        int[] array = {9, 0, 3, 2, 6, 5,
+                1, 8, 4, 8, 7, 2,
+                0, 5, 3, 9, 3, 5,
+                6, 5, 0, 8, 4, 1,
+                7, 2, 6, 2, 1, 9,
                 0, 1, 5, 7, 8, 4};
 
         ArrayGroupSort arrayGroupSort = new ArrayGroupSort(array, 2, 18);
@@ -26,6 +26,12 @@ public class Main {
         System.out.println("1x36:");
         arrayGroupSort.setGroupProperties(1, 36).print();
         arrayGroupSort.sort().print();
+        arrayGroupSort.orderBy(Order.DESC).sort().print();
+
+        System.out.println("Big:");
+        arrayGroupSort = new ArrayGroupSort(100, 100);
+        arrayGroupSort.fillRandom().print();
+        arrayGroupSort.orderBy(Order.ASC).sort().print();
         arrayGroupSort.orderBy(Order.DESC).sort().print();
     }
 }
